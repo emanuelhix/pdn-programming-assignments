252d6ca914fca5095a9cd0eb289f87fbdb6eaaab
555369 <hix0004@gpel11.cs.nor.ou.edu>
2025-08-31 18:27:29 -0500

    Initial commit of PG_0: C Refresher

---
+++ PG_0: C Refresher/run_all.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/bash
+
+cd Example/
+make -i
+make clean
+cd ..
+
+cd Merge/
+make -i
+make clean
+cd ..
+
+cd Quick/
+make -i
+make clean
+cd ..
+
+cd Bubble/
+make -i
+make clean
+cd ..
+
+cd Insertion/
+make -i
+make clean
+cd ..
+
+cd Selection/
+make -i
+make clean
+cd ..
+
+cd Statistics/
+make -i
+make clean
+cd ..
+
+cd Transpose/
+make -i
+make clean
+cd ..
+
+cd Histogram/
+make -i
+make clean
+cd ..
+
